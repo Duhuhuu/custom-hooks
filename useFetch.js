aec56cade9609150be91cf6efa78dbd87cfbd34e
Duhuhuu <ignisxmizu@gmail.com>
2022-09-02 17:26:57 -0600

    Comit de Hooks

---
+++ useFetch.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+
+
+export const useFetch = ( url ) => {
+
+    const [estate, setEstate] = useState({
+        data: null,
+        isLoading: true,
+        hasError: null,
+
+    })
+
+    const getFetch = async () =>{
+
+        setEstate({
+            ...estate,
+            isLoading: true,
+        });
+
+        const resp = await fetch(url);
+        const data  = await resp.json();
+        
+
+        setEstate({
+            data: data,
+            isLoading:false,
+            hasError: null,
+        });
+    }
+
+    useEffect(() => {
+        getFetch();
+        
+    }, [url])
+    
+
+  return { 
+    data:      estate.data, 
+    isLoading:  estate.isLoading,
+    hasError:   estate.hasError,
+ };
+}
